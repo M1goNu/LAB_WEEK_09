fd08f236d20dba9574ce44068b8aad5ea464f89f
M1goNu <rifqi.aldino71@gmail.com>
2025-11-05 11:12:06 +0700

    Commit No. 2 – State and Event Handler

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +16,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -43,26 +47,64 @@ class MainActivity : ComponentActivity() {
                     //and set it as the color of the surface
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val list = listOf("Tanu", "Tina", "Tono")
-                    //Here, we call the Home composable
-                    Home(list)
+                    Home()
                 }
             }
         }
     }
 }
 
-//Notice that we remove the @Preview annotation
-//this is because we're passing a parameter into the composable
-//When the compiler tries to build the preview,
-//it doesn't know what to pass into the composable
-//So, we create another composable function called PreviewHome
-//and we pass the list as a parameter
+
+//Declare a data class called Student
+data class Student(
+    var name: String
+)
 @Composable
-    fun Home(
-        //Here, we define a parameter called items
-        items: List<String>,
-    ) {
+fun Home() {
+    //Here, we create a mutable state list of Student
+    //We use remember to make the list remember its value
+    //This is so that the list won't be recreated when the composable recomposes
+    //We use mutableStateListOf to make the list mutable
+    //This is so that we can add or remove items from the list
+    //If you're still confused, this is basically the same concept as using
+    //useState in React
+    val listData = remember { mutableStateListOf(
+        Student("Tanu"),
+        Student("Tina"),
+        Student("Tono")
+    )}
+    //Here, we create a mutable state of Student
+    //This is so that we can get the value of the input field
+    val inputField = remember { mutableStateOf(Student("")) }
+
+    //We call the HomeContent composable
+    //Here, we pass:
+    //listData to show the list of items inside HomeContent
+    //inputField to show the input field value inside HomeContent
+    //A lambda function to update the value of the inputField
+    //A lambda function to add the inputField to the listData
+    HomeContent(
+        listData,
+        inputField.value,
+        { input -> inputField.value = inputField.value.copy(input) },
+        {
+            if (inputField.value.name.isNotBlank()) {
+                listData.add(inputField.value)
+                inputField.value = Student("")
+            }
+        }
+    )
+}
+
+//Here, we create a composable function called HomeContent
+//HomeContent is used to display the content of the Home composable
+@Composable
+    fun HomeContent(
+    listData: SnapshotStateList<Student>,
+    inputField: Student,
+    onInputValueChange: (String) -> Unit,
+    onButtonClick: () -> Unit
+) {
     //Here, we use LazyColumn to lazily display a list of items horizontally
     //LazyColumn is more efficient than Column
     //because it only composes and lays out the currently visible items
@@ -77,7 +119,7 @@ class MainActivity : ComponentActivity() {
                 //to add padding horizontally and vertically
                 //or Modifier.padding(start = 16.dp, top = 8.dp, end = 16.dp, bottom = 8.dp)
                 //to add padding to each side
-                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                modifier = Modifier.padding(30.dp).fillMaxSize(),
                 //Alignment.CenterHorizontally is used to align the Column horizontally
                 //You can also use verticalArrangement = Arrangement.Center to align the Column vertically
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -88,18 +130,27 @@ class MainActivity : ComponentActivity() {
                 //Here, we use TextField to display a text input field
                 TextField(
                     //Set the value of the input field
-                    value = "",
+                    value = inputField.name,
                     //Set the keyboard type of the input field
                     keyboardOptions = KeyboardOptions(
                         keyboardType = KeyboardType.Number
                     ),
                     //Set what happens when the value of the input field changes
                             onValueChange = {
-                    }
+                                //Here, we call the onInputValueChange lambda function
+                                //and pass the value of the input field as a parameter
+                                //This is so that we can update the value of the inputField
+                                onInputValueChange(it)
+                            }
                 )
                 //Here, we use Button to display a button
                 //the onClick parameter is used to set what happens when the button is clicked
-                Button(onClick = { }) {
+                Button(onClick = {
+                    //Here, we call the onButtonClick lambda function
+                    //This is so that we can add the inputField value to the listData
+                    //and reset the value of the inputField
+                    onButtonClick()
+                }) {
                     //Set the text of the button
                     Text(text = stringResource(
                         id = R.string.button_click)
@@ -109,12 +160,13 @@ class MainActivity : ComponentActivity() {
         }
         //Here, we use items to display a list of items inside the LazyColumn
         //This is the RecyclerView replacement
-        items(items) { item ->
+        //We pass the listData as a parameter
+        items(listData) { item ->
             Column(
                 modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = item)
+                Text(text = item.name)
             }
         }
     }
@@ -126,5 +178,5 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome() {
-    Home(listOf("Tanu", "Tina", "Tono"))
+    Home()
 }
